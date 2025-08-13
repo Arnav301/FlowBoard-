d3ab1fe50c1396256434dbb60a8755f9cb70a0ae
Sksham Kaushal <skshamkaushal@gmail.com>
2025-08-13 18:15:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+    <div className=" px-100 py-120 flex items-center justify-center text-5xl bg-amber-100">
+    <span className=" text-blue-500 flex items-center justify-center text-5xl border-l-amber-100"> HelloDarling </span>
+    </div>
+    </>
+  )
+}
+
+export default App
